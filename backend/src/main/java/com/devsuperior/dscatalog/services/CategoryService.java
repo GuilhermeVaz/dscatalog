b0e7569f63f0b2fb333e80daa653073b366ee445
Guilherme Vaz <guilherme_vaz@sicredi.com.br>
2022-10-04 11:40:57 -0300

    Pageable refactor

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -10,6 +10,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,16 +24,8 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
-        Page<Category> list = repository.findAll(pageRequest);
-        /*jeito sem lambda
-        List<CategoryDTO> listDto = new ArrayList<>();
-        for(Category cat : list){
-            listDto.add(new CategoryDTO(cat));
-        }
-        return listDto;*/
-        //jeito com lambda
-        //List<CategoryDTO> listDto = return
+    public Page<CategoryDTO> findAllPaged(Pageable pageable){
+        Page<Category> list = repository.findAll(pageable);
         return list.map(x -> new CategoryDTO(x));
     }
 
